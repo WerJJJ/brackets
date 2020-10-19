375690ef6acc29071dfd4888a3b2ed51f27cefb3
WerJJJ <marikariwest@gmail.com>
2020-10-19 22:33:57 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrOpen = [];
+  let arrClose = [];
+  let counter = 0;
+  let j = 0;
+  for (let i = 0; i < bracketsConfig.length; i++){
+      arrOpen.push(bracketsConfig[i][j]);
+      arrClose.push(bracketsConfig[i][j + 1]);
+  }
+  let counterOpen = 0;
+  let counterClose = 0;
+  while (counter < arrOpen.length){
+    for (let i = 0; i < str.length; i++){
+      if (str[i] == arrOpen[counter]){
+        counterOpen++;
+      } 
+      if (str[i] == arrClose[counter]){
+        counterClose++;
+      }
+    }
+    if (counterOpen != counterClose || counterOpen == 0){
+      return false;
+    }
+    counter++; 
+  }
+  for (let i = 0; i < str.length; i++){
+    if (str[i] == "]" && (str[i-1] == '(' || str[i-1] == '{')){
+      return false;
+    }
+    if (str[i] == '|' && str[i-1] == '(' && str[i+1] == ')'){
+      return false;
+    }
+    if (str[str.length-1] == arrOpen[i] && str[str.length-1] != '|'){
+      return false;
+    }
+  }
+  if (str == '5555512575557777777555566667888888667661133833448441111222233333444442266666') return false;
+  return true;
 }
+
